bb8555dbee2e1d552caf717c24b995269ce9a35f
shinwokkang <120699742+shinwokkang@users.noreply.github.com>
2025-01-23 00:33:40 +0900

    feat: gotologin 기능 추가

--- src/components/join/GotoLogin.tsx
+++ src/components/join/GotoLogin.tsx
@@ -1,7 +1,36 @@
+/** @jsxImportSource @emotion/react */
+import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
+
+const GotoLoginContainer = styled.div`
+  font-size: 16px;
+  color: gray;
+  display: flex;
+  justify-content: center;
+`;
+
+const UnderlinedLink = styled.u`
+  color: gray;
+  cursor: pointer;
+  text-decoration: underline;
+  justify-content: center;
+
+  &:hover {
+    color: black;
+  }
+`;
+
 const GotoLogin = () => {
+  const navigate = useNavigate();
+  const handleGotoLogin = () => {
+    navigate("/login"); // 로그인 후 이동할 경로
+  };
   return (
     <div>
-      이미 계정이 있으신가요? <u>로그인</u>
+      <GotoLoginContainer>
+        이미 계정이 있으신가요?
+        <UnderlinedLink onClick={handleGotoLogin}> 로그인</UnderlinedLink>
+      </GotoLoginContainer>
     </div>
   );
 };
